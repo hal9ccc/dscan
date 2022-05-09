fe655203f57758a8926c0f8a8b0b90a8df17bbf5
Matthias Schulze <matthias.schulze@outlook.de>
2022-05-09 13:52:42 +0200

    added MEDIA columns

--- db/table/MEDIA.sql
+++ db/table/MEDIA.sql
@@ -7,8 +7,17 @@ CREATE TABLE media (
   title          VARCHAR2(100)  NOT NULL,
   timestamp      timestamp      NOT NULL,
   idx            int            NOT NULL,
-  device         VARCHAR2(100)
+  device         VARCHAR2(100),
+  status         VARCHAR2(100),
+  comment        VARCHAR2(4000 char),
+  changed_at     timestamp with time zone,
+  deleted        number(1)
 );
+
+alter table media add  status         VARCHAR2(100);
+alter table media add  "COMMENT"      VARCHAR2(4000 char);
+alter table media add  changed_at     timestamp with time zone;
+alter table media add  deleted        number(1);
 
 
 ALTER TABLE media ADD (
